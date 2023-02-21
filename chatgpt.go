1772884be0db89d45aa5b9b81fb60585dae75bc2
Adam Derewecki <a@der.wiki>
2023-02-21 13:01:44 -0800

    Works

--- chatgpt.go
+++ chatgpt.go
@@ -9,8 +9,10 @@ import (
 
 func main() {
 	// Construct the request body
-	requestBody := map[string]string{
-		"text": "Write one paragraph teasers for 3 original Sherlock Holmes stories",
+	requestBody := map[string]interface{}{
+		"model":      "text-davinci-003",
+		"prompt":     "Write a one paragraph teaser for an original Sherlock Holmes stories",
+		"max_tokens": 100,
 	}
 	requestBodyBytes, err := json.Marshal(requestBody)
 	if err != nil {
@@ -21,20 +23,21 @@ func main() {
 	client := &http.Client{}
 
 	// Create a new HTTP request
-	request, err := http.NewRequest("POST", "https://api.openai.com/v1/engine/davinci-codex/completions", bytes.NewBuffer(requestBodyBytes))
+	request, err := http.NewRequest("POST", "https://api.openai.com/v1/completions", bytes.NewBuffer(requestBodyBytes))
 	if err != nil {
 		panic(err)
 	}
 
 	// Set the authorization header using your API key
-	request.Header.Set("Authorization", "Bearer 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")
+	request.Header.Set("Authorization", "Bearer ")
+	request.Header.Set("Content-Type", "application/json")
 
 	// Send the HTTP request to the API endpoint
 	response, err := client.Do(request)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(response)
+	fmt.Println(fmt.Sprintln("Response:\n%s", response))
 
 	// Read the response body
 	var responseBody map[string]interface{}
@@ -45,4 +48,5 @@ func main() {
 
 	// Print the generated response
 	fmt.Println(responseBody["choices"]) // .([]interface{})[0].(map[string]interface{})["text"])
+	fmt.Println(responseBody["text"])    // .([]interface{})[0].(map[string]interface{})["text"])
 }
