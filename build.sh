a762a4ef57833c75c52551e83097c4b5fe9742c0
Adam Derewecki <a@der.wiki>
2023-04-13 08:34:37 -0700

    Add build script

--- build.sh
+++ build.sh
@@ -2,23 +2,23 @@
 
 # Build for Windows (64-bit)
 echo "Building for Windows (64-bit)"
-GOOS=windows GOARCH=amd64 go build -o chatgpt.exe chatgpt.go
+GOOS=windows GOARCH=amd64 go build -o release/chatgpt.exe chatgpt.go
 
 # Build for Linux (64-bit)
 echo "Building for Linux (64-bit)"
-GOOS=linux GOARCH=amd64 go build -o chatgpt-linux chatgpt.go
+GOOS=linux GOARCH=amd64 go build -o release/chatgpt-linux chatgpt.go
 
 # Build for macOS (64-bit)
 echo "Building for macOS (64-bit)"
-GOOS=darwin GOARCH=amd64 go build -o chatgpt-macos chatgpt.go
+GOOS=darwin GOARCH=amd64 go build -o release/chatgpt-macos chatgpt.go
 
 # Build for FreeBSD (64-bit)
 echo "Building for FreeBSD (64-bit)"
-GOOS=freebsd GOARCH=amd64 go build -o chatgpt-freebsd chatgpt.go
+GOOS=freebsd GOARCH=amd64 go build -o release/chatgpt-freebsd chatgpt.go
 
 # Build for OpenBSD (64-bit)
 echo "Building for OpenBSD (64-bit)"
-GOOS=openbsd GOARCH=amd64 go build -o chatgpt-openbsd chatgpt.go
+GOOS=openbsd GOARCH=amd64 go build -o release/chatgpt-openbsd chatgpt.go
 
 echo "Done!"
 
